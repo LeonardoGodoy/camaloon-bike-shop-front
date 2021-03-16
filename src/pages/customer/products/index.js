967307bcc724cad5dfb90b01fd646db01493709b
Leonardo Godoy <leonardo.godoy@trackmob.com.br>
2021-03-16 10:50:17 -0300

    Implement customer products page WIP

--- src/pages/customer/products/index.js
+++ src/pages/customer/products/index.js
@@ -1,5 +1,49 @@
+import React from 'react';
+import { useHistory } from "react-router-dom";
+
+import useFetch from './../../../hooks/useFetch';
+import { fetchProducts } from './../../../adapters/api';
+import Loader from './../../../components/Loader';
+
+function Product({ product }) {
+  const history = useHistory();
+
+  const handleProductClick =({ id }) => {
+    let path = `/products/${id}`;
+    history.push(path);
+  }
+
+  return (
+    <div key={product.id} class='product-card' onClick={() => handleProductClick(product)}>
+      <img class="product-card__img" src="https://source.unsplash.com/190x190/?bycicle,bikeshop" alt="bike"/>
+      <p class="product-card__title">{product.title}</p>
+    </div>
+  );
+}
+
 function ProductsIndex() {
-  return <h2>ProductsIndex</h2>;
+  const { isLoaded, response, error } = useFetch(fetchProducts())
+
+  let content;
+  if (error) {
+    content = <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    content = <Loader />;
+  } else {
+    content = (
+      <div class='product-list'>
+        {response.map(product => (
+          <Product product={product}/>
+        ))}
+      </div>
+    )
+  }
+
+  return (
+    <div class="page--center">
+      {content}
+    </div>
+  );
 }
 
 export default ProductsIndex;
